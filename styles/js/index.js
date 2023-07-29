c3f25202430710a4614492c1544da55c0f81ea36
Жанарбай <satkynovaz@gmail.com>
2023-07-29 15:59:53 +0600

    lll

---
+++ styles/js/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        // slidesToScroll: 3,
+        speed: 1000,
+        easing: 'ease',
+        // infinite: false,
+        initialSlide: 1,
+        // autoplay: true,
+        // autoplaySpeed: 500,
+        // pauseOnFocus: true,
+        pauseOnHover: true,
+        // pauseOnDotsHover: true,
+        // draggable: false, 
+        // swipe: true,
+        // touchThreshold: 5,
+        // touchMove: false,
+        centerMode: false,
+        waitForAnimate: false,
+        variableWidth: false,
+        // rows: 2,
+        // slidesPerRow: 2,
+        // vertical: true,
+        // verticalSwiping: true,
+        
+    });
+});
